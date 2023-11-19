f6f3c36fe044ed47eb5f923b18d3c92b9ca00fa1
Bilal Shakeel <info.bilalshakeel@gmail.com>
2023-11-19 10:52:09 +0500

    random number method added

---
+++ Assignments/Max-Sub-Array TC/max-sub-array.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <string>
+#include<chrono>
+#include <limits>
+
+using namespace std;
+
+
+int generateRandomNum() {
+    return rand() % 2001 - 1000;
+}
