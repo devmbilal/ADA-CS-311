7fe0d8e981b180257a4f14d6c0b1a88750515582
Bilal Shakeel <info.bilalshakeel@gmail.com>
2023-11-20 10:52:44 +0500

    create file method added

--- Assignments/Max-Sub-Array TC/max-sub-array.cpp
+++ Assignments/Max-Sub-Array TC/max-sub-array.cpp
@@ -11,3 +11,19 @@ using namespace std;
 int generateRandomNum() {
     return rand() % 2001 - 1000;
 }
+
+void createFile(int n) {
+    string filename = "file_" + to_string(n) + ".txt";
+
+    ofstream file(filename);
+
+    file << n << endl;
+
+    for (int i = 0; i < n; ++i) {
+        int num = generateRandomNum();
+        if (num < 0)
+        file << num << " ";
+    }
+
+    file.close();
+}
