5b7eda31881a6ffb4e2bd5a5c1b3ab353caa54bb
Bilal Shakeel <info.bilalshakeel@gmail.com>
2023-11-24 10:55:29 +0500

    main added

--- Assignments/Max-Sub-Array TC/max-sub-array.cpp
+++ Assignments/Max-Sub-Array TC/max-sub-array.cpp
@@ -87,3 +87,50 @@ int maxSubArrayDC(int A[], int low, int high)
 
     return maximum(maximum(left, right), crossings);
 }
+
+
+
+int main() {
+
+    srand(time(0));
+
+    int sizes[] = { 10, 15, 20, 30, 40, 50, 60, 70, 80, 100, 100, 200, 250, 300, 350, 400, 450, 500, 750, 1000 };
+
+    for (int size : sizes) {
+        createFile(size);
+    }
+
+    for (int size : sizes) {
+        string filename = "file_" + to_string(size) + ".txt";
+        ifstream file(filename);
+        int n;
+        file >> n;
+
+        int* nums = new int[n];
+        for (int i = 0; i < n; ++i) {
+            file >> nums[i];
+        }
+
+        file.close();
+
+        auto start = chrono::high_resolution_clock::now();
+        // Find maximum subarray using Brute Force approach
+        int maxSumBF = maxSubArrayBF(nums, n);
+        auto end = chrono::high_resolution_clock::now();
+        auto durationBF = chrono::duration_cast<chrono::microseconds>(end - start).count();
+
+        start = chrono::high_resolution_clock::now();
+        // Find maximum subarray using Divide and Conquer approach
+        int maxSumDC = maxSubArrayDC(nums, 0, n - 1);
+        end = chrono::high_resolution_clock::now();
+        auto durationDC = chrono::duration_cast<chrono::microseconds>(end - start).count();
+
+        delete[] nums; // Deallocate the dynamically allocated array
+
+        cout << "File size: " << size << endl;
+        cout << "Brute Force Max Sum: " << maxSumBF << " Execution Time: " << durationBF << " microseconds" << endl;
+        cout << "Divide and Conquer Max Sum: " << maxSumDC << " Execution Time: " << durationDC << " microseconds" << endl;
+    }
+
+    return 0;
+}
